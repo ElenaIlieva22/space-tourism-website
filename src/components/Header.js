015426d4230237321a17858e99c079553b30fbb8
StoleB <stole.boskov@gmail.com>
2024-08-01 07:22:29 +0200

    Dwstination

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
   return (
-    <header className='container'>
+    <header className='header-container'>      
       <nav className='nav'>
         <ul className='links'>
-          <li><Link to="/">00 HOME</Link></li>
-          <li><Link to="/destination">01 DESTINATION</Link></li>
-          <li><Link to="/crew">02 CREW</Link></li>
-          <li><Link to="/technology">03 TECHNOLOGY</Link></li>
+          <li><NavLink to="/" activeClassName="active">00 HOME</NavLink></li>
+          <li><NavLink to="/destination" activeClassName="active">01 DESTINATION</NavLink></li>
+          <li><NavLink to="/crew" activeClassName="active">02 CREW</NavLink></li>
+          <li><NavLink to="/technology" activeClassName="active">03 TECHNOLOGY</NavLink></li>
         </ul>
       </nav>
     </header>
