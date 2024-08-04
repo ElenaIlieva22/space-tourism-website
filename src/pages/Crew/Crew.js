9d98cc574461ddf328e6611fef770a780f83e949
ElenaIlieva22 <elena.nikolova22@gmail.com>
2024-08-04 21:39:32 +0200

    starting technology page

--- src/pages/Crew/Crew.js
+++ src/pages/Crew/Crew.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import './Crew.css'
+import React from "react";
+import "./Crew.css";
 
 const Crew = () => {
-    return (
-        <div className="crew-container">
-            <div className='crew-background'>
-                <img src='./images\crew\background-crew-desktop.jpg'className="crew-bg" alt="Bitmap Copy"/>
-            </div>
-            <div className='crew-content'>
-                <p className='crew-heading' >02 MEET YOUR CREW</p>
-                <h4 className="crew-function">COMMANDER</h4>
-                <h2 className="crew-name">DOUGLAS HURLEY</h2>
-                <p className="crew-text">Douglas Gerald Hurley is an American engineer, former Marine Corps pilot 
-                    and former NASA astronaut. He launched into space for the third time as 
-                    commander of Crew Dragon Demo-2.
-                </p>
-            </div>
-            <div className='crew-pic'>
-                <img src='./images\crew\image-douglas-hurley.png'className="crew-douglas-hurley" alt="Team member Douglas Hurley"/>
-            </div>
+  return (
+    <div className="crew-container">
+      <div className="crew-background">
+        <img
+          src="./images/crew/background-crew-desktop.jpg"
+          className="crew-bg"
+          alt="Bitmap Copy"
+        />
+      </div>
+      <div className="crew-content" >
+        <div className="crew-inscription" style={{width: "30%"}}>
+          <p className="crew-heading">02 MEET YOUR CREW</p>
+          <h4 className="crew-function">COMMANDER</h4>
+          <h2 className="crew-name">DOUGLAS HURLEY</h2>
+          <p className="crew-text">
+            Douglas Gerald Hurley is an American engineer, former Marine Corps
+            pilot and former NASA astronaut. He launched into space for the
+            third time as commander of Crew Dragon Demo-2.
+          </p>
         </div>
-    )
-}
+
+        <div className="crew-pic">
+          <img
+            src="./images/crew/image-douglas-hurley.png"
+            className="crew-douglas-hurley"
+            alt="Team member Douglas Hurley"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Crew;
---
+++ src/pages/Technology/Technology.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./Technology.css";
+
+const Technology = () => {
+  return (
+    <div className="tech-container">
+      {/* <div className="tech-background">
+        <img
+          src="./images/technology/background-technology-desktop.jpg"
+          className="tech-bg"
+          alt="Techonoly background"
+        />
+      </div> */}
+
+      <div className="tech-content">
+        <div>
+          <div className="tech-heading">
+            <p>03 SPACE LAUNCH 101</p>
+          </div>
+          <div className="tech-inscription">
+            <div>
+              <p>buttons</p>
+            </div>
+            <div>
+              <p className="tech-terminology">THE TERMINOLOGY...</p>
+              <h2 className="tech-title">LAUNCH VECHICLE</h2>
+              <p className="tech-text">
+                A launch vehicle or carrier rocket is a rocket-propelled vehicle
+                used to carry a payload from Earth's surface to space, usually
+                to Earth orbit or beyond. Our WEB-X carrier rocket is the most
+                powerful in operation. Standing 150 metres tall, it's quite an
+                awe-inspiring sight on the launch pad!
+              </p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <img
+            src="./images/technology/image-launch-vehicle-portrait.jpg"
+            alt="Launch vehicle"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Technology;
