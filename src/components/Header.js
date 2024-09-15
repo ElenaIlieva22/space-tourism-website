0d1e74a599845078a959044ecd1183dd16ded18b
StoleB <stole.boskov@gmail.com>
2024-09-15 20:09:08 +0200

    Menu Done

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,14 +13,14 @@ const Header = () => {
     <header className='header-container'>      
       <nav className={`nav-menu ${isMenuOpen ? 'open' : ''}`}>
         <ul className='links'>
-          <li><span class="number">00</span><NavLink to="/" activeClassName="active" >HOME</NavLink></li>
-          <li><span class="number">01</span><NavLink to="/destination" activeClassName="active">DESTINATION</NavLink></li>
-          <li><span class="number">02</span><NavLink to="/crew" activeClassName="active">CREW</NavLink></li>
-          <li><span class="number">03</span><NavLink to="/technology" activeClassName="active">TECHNOLOGY</NavLink></li>
+          <li><span className="number">00</span><NavLink to="/" activeClassName="active" >HOME</NavLink></li>
+          <li><span className="number">01</span><NavLink to="/destination" activeClassName="active">DESTINATION</NavLink></li>
+          <li><span className="number">02</span><NavLink to="/crew" activeClassName="active">CREW</NavLink></li>
+          <li><span className="number">03</span><NavLink to="/technology" activeClassName="active">TECHNOLOGY</NavLink></li>
         </ul>
       </nav>
-      <button className="menu-toggle" onClick={toggleMenu}>
-        ☰
+      <button className={`menu-toggle ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+        {isMenuOpen ? '✕' : '☰'}
       </button>
     </header>
   );
